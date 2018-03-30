8b1d606fa108c92e6efd187a6e83229a12eef4a8
Josh White <joshcrwhite@icloud.com>
2018-03-29 20:57:04 -0400

    working woohoo

---
+++ index.js
@@ -0,0 +1,34 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  io.emit('data', req.body);
+  res.sendStatus(200);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
