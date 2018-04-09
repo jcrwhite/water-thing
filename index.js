b2bced663617eed27b8ccb63181ce55f2049288b
Josh White <joshcrwhite@icloud.com>
2018-04-08 23:50:56 -0400

    added chart

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,9 +17,20 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+const formatData = (data) => {
+  let count = 249;
+  return _.map(_.drop(data.split('+').filter(Boolean)), (d) => {
+    count++;
+    return {
+      x: count,
+      y: d.split('%')[0]
+    }
+  });
+}
+
 app.post('/', (req, res) => {
-  console.log(req.body);
-  io.emit('data', req.body);
+  // console.log(req.body);
+  io.emit('data', formatData(req.body));
   res.sendStatus(200);
 });
 
